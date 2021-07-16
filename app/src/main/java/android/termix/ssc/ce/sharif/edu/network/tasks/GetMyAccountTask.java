81d6b627fbfb32506aa72b84eed683d2fa42d729
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2021-07-16 15:02:36 +0430

    Add Network Tasks

---
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/network/tasks/GetMyAccountTask.java
@@ -0,0 +1,22 @@
+package android.termix.ssc.ce.sharif.edu.network.tasks;
+
+import android.termix.ssc.ce.sharif.edu.network.NetworkTask;
+
+import okhttp3.HttpUrl;
+
+/**
+ * @author AryanAhadinia
+ * @since 1
+ */
+public abstract class GetMyAccountTask extends NetworkTask {
+
+    @Override
+    protected HttpUrl getURL() {
+        return null;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
