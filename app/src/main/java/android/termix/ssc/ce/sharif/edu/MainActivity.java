fc1df6d5e8cebe171eb2532fd6d8864361b1d91d
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2021-07-16 14:26:54 +0430

    Initial commit

---
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/MainActivity.java
@@ -0,0 +1,14 @@
+package android.termix.ssc.ce.sharif.edu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
