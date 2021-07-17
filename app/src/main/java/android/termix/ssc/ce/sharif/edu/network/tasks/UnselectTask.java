98029c6146f0502a9d53cf2e6c282f352479e30b
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2021-07-17 19:02:24 +0430

    Add UnselectTask

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/network/tasks/UnselectTask.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/network/tasks/UnselectTask.java
@@ -1,15 +1,19 @@
 package android.termix.ssc.ce.sharif.edu.network.tasks;
 
+import android.termix.ssc.ce.sharif.edu.model.Account;
+import android.termix.ssc.ce.sharif.edu.network.NetworkException;
 import android.termix.ssc.ce.sharif.edu.network.NetworkTask;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -17,8 +21,8 @@ import okhttp3.Response;
  * @since 1
  */
 public abstract class UnselectTask extends NetworkTask {
-    private int courseId;
-    private int groupId;
+    private final int courseId;
+    private final int groupId;
 
     public UnselectTask(int courseId, int groupId) {
         this.courseId = courseId;
@@ -32,7 +36,11 @@ public abstract class UnselectTask extends NetworkTask {
 
     @Override
     public void run() {
-        getOkHttpClient().newCall(getRequest()).enqueue(new Callback() {
+        RequestBody requestBody = new FormBody.Builder()
+                .add("courseId", String.valueOf(courseId))
+                .add("groupId", String.valueOf(groupId))
+                .build();
+        getOkHttpClient().newCall(getDeleteRequest(requestBody)).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
                 onError(e);
@@ -40,7 +48,17 @@ public abstract class UnselectTask extends NetworkTask {
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) {
-
+                if (response.isSuccessful()) {
+                    onResult(null);
+                } else {
+                    final HashMap<Integer, String> errorMessages = new HashMap<>();
+                    errorMessages.put(400, "درخواست شما معتبر نیست.");
+                    errorMessages.put(403, "حساب کاربری تایید شده نیست.");
+                    errorMessages.put(500, "مشکلات داخلی سرور، لطفا مجددا تلاش کنید.");
+                    onException(new NetworkException(errorMessages.getOrDefault(response.code(),
+                            "دوباره تلاش کنید."),
+                            response.code()));
+                }
             }
         });
     }
