e2dd0092513f3a2c25e9406faed4591ea4bd4672
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2021-07-17 17:14:37 +0430

    Modify App class

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/App.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/App.java
@@ -1,21 +1,16 @@
 package android.termix.ssc.ce.sharif.edu;
 
 import android.app.Application;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.termix.ssc.ce.sharif.edu.network.CookieManager;
 
+/**
+ * @author AryanAhadinia
+ * @since 1
+ */
 public class App extends Application {
-
     @Override
     public void onCreate() {
         super.onCreate();
-
-    }
-
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-        System.out.println(CookieManager.getInstance().getCookies());
+        CookieManager.init(getApplicationContext());
     }
 }
