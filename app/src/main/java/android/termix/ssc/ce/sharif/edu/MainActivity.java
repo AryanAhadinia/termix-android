41a6d0c1ff9dc5309c336dfc636592a4be482843
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2021-07-18 19:07:28 +0430

    Add Animation to background

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/MainActivity.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.termix.ssc.ce.sharif.edu.network.NetworkException;
+import android.termix.ssc.ce.sharif.edu.network.tasks.GetAllCoursesTask;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -25,34 +27,16 @@ import java.util.Objects;
 import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String[] PERMISSIONS = {Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO};
+    public final static String[] PERMISSIONS = {Manifest.permission.INTERNET,
+            Manifest.permission.RECORD_AUDIO};
 
     private ConstraintLayout constraintLayout;
     private TextInputLayout textInputLayout;
     private AutoCompleteTextView searchBar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        constraintLayout = findViewById(R.id.constraint_layout);
-        textInputLayout = findViewById(R.id.text_input_layout);
-        searchBar = findViewById(R.id.search_bar);
-
-        textInputLayout.setStartIconOnClickListener(e -> mPermissionResult.launch(PERMISSIONS));
-
-        searchBar.setOnLongClickListener(e -> {
-            // TODO: all courses dialog
-            return true;
-        });
-
-        //        setUpSettings(); // TODO
-
-    }
-
-    private final ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(new
-            ActivityResultContracts.StartActivityForResult(), result -> {
+    private final ActivityResultLauncher<Intent> resultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
         if (result.getResultCode() == Activity.RESULT_OK) {
             if (result.getData() != null) {
                 ArrayList<String> finalResults = result.getData()
@@ -74,6 +58,31 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().setReorderingAllowed(true)
+//                    .add(R.id.fragment_container_view, LoadingFragment.class, null).commit();
+//        }
+        setContentView(R.layout.activity_main);
+
+        constraintLayout = findViewById(R.id.constraint_layout);
+        textInputLayout = findViewById(R.id.text_input_layout);
+        searchBar = findViewById(R.id.search_bar);
+
+        textInputLayout.setStartIconOnClickListener(view -> mPermissionResult.launch(PERMISSIONS));
+
+        searchBar.setOnLongClickListener(view -> {
+            // TODO: all courses dialog
+            return true;
+        });
+
+        //        setUpSettings(); // TODO
+
+        Intent intent = new Intent(this, LoginSignupActivity.class);
+        startActivity(intent);
+    }
 
     private void setUpVoiceSearch() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
