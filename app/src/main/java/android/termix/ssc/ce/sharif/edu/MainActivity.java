0d04f28b19987cec5f6a4244cf3ce09497a1d2e0
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2021-07-20 12:04:27 +0430

    Fix MainActivity status bar

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/MainActivity.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/MainActivity.java
@@ -13,6 +13,8 @@ import android.termix.ssc.ce.sharif.edu.network.tasks.TestTokenTask;
 import android.termix.ssc.ce.sharif.edu.scheduleUI.AllCoursesDialogFragment;
 import android.termix.ssc.ce.sharif.edu.scheduleUI.DayAdapter;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
@@ -44,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         new TestTokenTask() {
             @Override
