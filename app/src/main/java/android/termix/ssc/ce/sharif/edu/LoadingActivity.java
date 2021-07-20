0bdcfc529b48342075ebad6fcf70f71ee549b291
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2021-07-20 11:51:47 +0430

    Add showing content behind status bar

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/LoadingActivity.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/LoadingActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.termix.ssc.ce.sharif.edu.network.NetworkException;
 import android.termix.ssc.ce.sharif.edu.network.tasks.TestTokenTask;
+import android.view.View;
 import android.widget.ImageView;
 
 public class LoadingActivity extends AppCompatActivity {
@@ -15,6 +16,8 @@ public class LoadingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setContentView(R.layout.activity_loading);
         ImageView logo = findViewById(R.id.logo);
         ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
