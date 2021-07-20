96356dba31ed4dcf9992bdaada58d24b932692c4
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2021-07-20 17:01:35 +0430

    Set status and navigation bar color

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/MainActivity.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/MainActivity.java
@@ -48,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.dark_red));
 
         new TestTokenTask() {
             @Override
