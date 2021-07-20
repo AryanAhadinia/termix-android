42d7015c1171b465b034d26ad3b4c3a5862fdb3c
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2021-07-20 11:55:30 +0430

    Add showing content behind navigation bar

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/LoadingActivity.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/LoadingActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.termix.ssc.ce.sharif.edu.network.NetworkException;
 import android.termix.ssc.ce.sharif.edu.network.tasks.TestTokenTask;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class LoadingActivity extends AppCompatActivity {
@@ -18,6 +19,8 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_loading);
         ImageView logo = findViewById(R.id.logo);
         ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
--- app/src/main/java/android/termix/ssc/ce/sharif/edu/LoginSignupActivity.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/LoginSignupActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -32,6 +33,8 @@ public class LoginSignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.login_sign_up_page);
 
         loginSignupActivityWeakReference = new WeakReference<>(this);
