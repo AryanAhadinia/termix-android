30b592cbaea130a7096cc20701ee8d1cdd6c7e7a
shahabhm <shhm3834@gmail.com>
2021-07-21 23:53:50 +0430

    bind reset password link to ServeForgetPasswordActivity

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/ServeForgetPasswordActivity.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/ServeForgetPasswordActivity.java
@@ -1,6 +1,21 @@
 package android.termix.ssc.ce.sharif.edu;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ServeForgetPasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
 }
