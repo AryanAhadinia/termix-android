a4a0444576c2a23638f0921b8e7d13142faf122a
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2021-07-21 20:15:53 +0430

    Change gradiant

---
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/AccountVerificationActivity.java
@@ -0,0 +1,17 @@
+package android.termix.ssc.ce.sharif.edu;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountVerificationActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+
+    }
+}
