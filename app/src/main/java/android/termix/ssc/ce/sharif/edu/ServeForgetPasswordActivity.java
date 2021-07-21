033889d1f12f849db4e9fa8c49eaf7261d1da399
shahabhm <shhm3834@gmail.com>
2021-07-21 23:58:59 +0430

    get token from reset password link

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/ServeForgetPasswordActivity.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/ServeForgetPasswordActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.time.Duration;
 
 public class ServeForgetPasswordActivity extends AppCompatActivity {
     @Override
@@ -17,5 +20,10 @@ public class ServeForgetPasswordActivity extends AppCompatActivity {
         Intent appLinkIntent = getIntent();
         String appLinkAction = appLinkIntent.getAction();
         Uri appLinkData = appLinkIntent.getData();
+        if (appLinkAction != null){
+            String token = appLinkData.getLastPathSegment();
+            Toast toast = Toast.makeText(getApplicationContext(), "token: "+ token, Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
