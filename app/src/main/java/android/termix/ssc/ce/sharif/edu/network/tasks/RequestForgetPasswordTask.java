5db6ea7df6a302c8cd0414db461a306287379c83
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2021-07-21 22:43:04 +0430

    Add RequestForgetPasswordTask

---
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/network/tasks/RequestForgetPasswordTask.java
@@ -0,0 +1,57 @@
+package android.termix.ssc.ce.sharif.edu.network.tasks;
+
+import android.termix.ssc.ce.sharif.edu.network.NetworkException;
+import android.termix.ssc.ce.sharif.edu.network.NetworkTask;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public abstract class RequestForgetPasswordTask extends NetworkTask<Object> {
+    private final String email;
+
+    public RequestForgetPasswordTask(String email, String password) {
+        this.email = email;
+    }
+
+    @Override
+    protected HttpUrl getURL() {
+        return HttpUrl.parse(getServerUrl().concat("/api/user/forget_password/request")).newBuilder().build();
+    }
+
+    @Override
+    public void run() {
+        RequestBody requestBody = new FormBody.Builder()
+                .add("email", email)
+                .build();
+        getOkHttpClient().newCall(getPostRequest(requestBody)).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                onError(e);
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) {
+                if (response.isSuccessful()) {
+                    onResult(null);
+                } else {
+                    final HashMap<Integer, String> errorMessages = new HashMap<>();
+                    errorMessages.put(400, "درخواست شما معتبر نیست.");
+                    errorMessages.put(401, "چنین حسابی وجود ندارد.");
+                    errorMessages.put(500, "مشکلات داخلی سرور، لطفا مجددا تلاش کنید.");
+                    onException(new NetworkException(errorMessages.getOrDefault(response.code(),
+                            "دوباره تلاش کنید."),
+                            response.code()));
+                }
+            }
+        });
+    }
+}
