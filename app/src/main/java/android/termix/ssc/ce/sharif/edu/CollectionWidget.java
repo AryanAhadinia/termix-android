2f9f09ff1ef1b3d927c1da179213b2276e4c85bf
shahabhm <shhm3834@gmail.com>
2021-07-23 01:56:36 +0430

    add widget update [not checked]. refactor.

--- app/src/main/java/android/termix/ssc/ce/sharif/edu/CollectionWidget.java
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/CollectionWidget.java
@@ -2,20 +2,29 @@ package android.termix.ssc.ce.sharif.edu;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
-
-import androidx.annotation.NonNull;
-
 import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
 //com
 public class CollectionWidget extends AppWidgetProvider {
+    public static final String ACTION_AUTO_UPDATE = "AUTO_UPDATE";
+    AppWidgetAlarm appWidgetAlarm;
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_list);
         setRemoteAdapter(context, views);
         appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    private static void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, WidgetService.class));
     }
 
     @Override
@@ -29,15 +38,29 @@ public class CollectionWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         Toast.makeText(context, "onEnabled called", Toast.LENGTH_LONG).show();
+        appWidgetAlarm = new AppWidgetAlarm(context.getApplicationContext());
+        appWidgetAlarm.startAlarm();
     }
 
     @Override
     public void onDisabled(Context context) {
         Toast.makeText(context, "onDisabled called", Toast.LENGTH_LONG).show();
+        appWidgetAlarm.stopAlarm();
     }
 
-    private static void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
-        views.setRemoteAdapter(R.id.widget_list,
-                new Intent(context, WidgetService.class));
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (intent.getAction().equals(ACTION_AUTO_UPDATE)) {
+            Intent i = new Intent(context, CollectionWidget.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+// Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
+// since it seems the onUpdate() is only fired on that:
+            int[] ids = AppWidgetManager.getInstance(context.getApplicationContext()).getAppWidgetIds(
+                    new ComponentName(context.getApplicationContext(), CollectionWidget.class));
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+            context.sendBroadcast(i);
+        }
     }
 }
