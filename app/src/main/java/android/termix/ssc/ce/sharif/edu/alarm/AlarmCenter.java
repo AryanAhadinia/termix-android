6f6432402f24090606ec828a2bf10c237800a854
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2021-07-24 22:26:41 +0430

    sd

---
+++ app/src/main/java/android/termix/ssc/ce/sharif/edu/alarm/AlarmCenter.java
@@ -0,0 +1,6 @@
+package android.termix.ssc.ce.sharif.edu.alarm;
+
+public class AlarmCenter {
+    public static final String PREFERENCE_NAME = "alarm";
+    public static final String PREFERENCE_LABEL = "alarmOffset"; // "alarmOffset_40429_1
+}
